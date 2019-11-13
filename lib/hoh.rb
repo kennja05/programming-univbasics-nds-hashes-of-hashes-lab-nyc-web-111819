69175428773c1a935af8364bbd6211cc1992e85d
Jacob Kenny <jacobkenny05@gmail.com>
2019-11-13 17:27:46 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,11 +46,21 @@ def naming_system
    sub_category: {
      label: "Phylum",
      sub_category: {
-       label: "Class"
-     }
-     {
-   label: "Order"
-  }
-   
+       label: "Class",
+         sub_category: {
+           label: "Order", 
+             sub_category: {
+               label: "Family",
+                 sub_category: {
+                   label: "Genus", 
+                     sub_category: {
+                       label: "Species"
+                     }
+                 }
+             }
+         }
+     
+  
+   }
  }
 end
